77477ed87a17bbb124b63041ddc99b27ade14799
Jonathan Breedlove <picantegamer@gmail.com>
2011-11-18 22:29:59 -0800

    Initial DAL (Data Abstraction Layer)

---
+++ dal/mysql.php
@@ -0,0 +1,150 @@
+<?php
+/*
+ * Prodigy Shared Learning System
+ * MySQL Database Abstraction Layer
+ *
+ */
+require_once("../conf.php");
+
+class mysqlDAL{
+
+    //************************************************************************************
+    //addClass
+    //adds a new class to the database
+    //
+    public function addClass($name, $description){
+        global $db_host, $db_name, $db_user, $db_pass;
+
+    	try {
+    		$dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+
+    		$createdDate = time();
+
+    		$dbh->exec("INSERT INTO class(name, description, createdDate)
+                VALUES ('$name', '$description', '$createdDate')");
+
+    		echo ("Added class ".$name);
+
+    		$dbh = NULL;
+    	}catch(PDOException $e){
+    		echo $e->getMessage();
+    	}
+
+    }
+
+
+    //************************************************************************************
+    //addLesson
+    //adds a new lesson to the database
+    //
+    public function addLesson($classID, $lessonNum, $name, $description, $content){
+        global $db_host, $db_name, $db_user, $db_pass;
+
+        try {
+            $dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+
+            $createdDate = time();
+
+            $dbh->exec("INSERT INTO lesson(classID, lessonNum, name, description, content)
+                VALUES ('$classID', '$lessonNum', $name', '$description', '$content')");
+
+            echo ("Added lesson ".$name);
+
+            $dbh = NULL;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+
+    }
+
+    //************************************************************************************
+    //getClassList
+    //retrieves a list of classes from the database
+    //
+    public function getClassList(){
+        global $db_host, $db_name, $db_user, $db_pass;
+
+    	try {
+    		$dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+
+    		$sql = "SELECT * FROM class";
+
+    		return $dbh->query($sql);
+
+    		$dbh = NULL;
+    	}catch(PDOException $e){
+    		echo $e->getMessage();
+    	}
+    }
+
+    //************************************************************************************
+    //getLessonList
+    //retrieves a list of lessons from the database
+    //
+    public function getLessonList(){
+        global $db_host, $db_name, $db_user, $db_pass;
+
+        try {
+            $dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+
+            $sql = "SELECT * FROM lesson";
+
+            return $dbh->query($sql);
+
+            $dbh = NULL;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+
+    //************************************************************************************
+    //getClass
+    //retrieves a single class from the database
+    //
+    public function getClass($cID){
+        global $db_host, $db_name, $db_user, $db_pass;
+
+        try {
+            $dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+
+            $sql = "SELECT * FROM class WHERE classID='$cID'";
+
+            return $dbh->query($sql);
+
+            $dbh = NULL;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+
+
+    //************************************************************************************
+    //getLesson
+    //retrieves a single lesson from the database
+    //
+    public function getLesson($lID){
+        global $db_host, $db_name, $db_user, $db_pass;
+
+        try {
+            $dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+
+            $sql = "SELECT * FROM lesson WHERE lessonID='$lID'";
+
+            return $dbh->query($sql);
+
+            $dbh = NULL;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+}
+
+$prodigyDB = new mysqlDAL;
+
+foreach ($prodigyDB->getLesson(1) as $row){
+    echo $row['name'] . "<br/>";
+}
+
+//$prodigyDB->addClass("Cajun Style Cooking", "Once you take this class, you'll be able to cook Southern favorites such as fried catfish like a pro!");
+
+?>
