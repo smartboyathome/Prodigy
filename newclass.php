ef1a3679e634d8aaebb416faf1bae07956536a50
Mike Wiser <wiser.mike@gmail.com>
2011-11-18 22:09:18 -0800

    Added Clear button to newclass.php

--- newclass.php
+++ newclass.php
@@ -62,23 +62,17 @@ if (!$db_select){
     </nav>
 
     <div id="wrapper" class="frame">
-    
-	    <div align="right" style="padding:15px; color: #437EA1; ">
-		<input type="text" align:right; name="search" /><a href=""> Search!</a>
-	    </div>
 	    
 	    <div align="center" class="classpage" style="height: auto;">
 		<form name="input" action="html_form_action.asp" method="get">
 		    <div style="font-size: 20px; padding: 15px; font-weight: bold;">Welcome! Create a new class by entering a class name and description.</div>
 		    Class Name: <input type="text" name="user" style="border-radius: 2px;"/> <br> <br>
 		    Description: <input type="text" size="80" name="description" style="border-radius: 2px;" />
-		    <br> <input type="submit" value="Create Class!" style="margin: 5px;"/>
+		    <br> <input type="submit" value="Create Class" style="margin: 5px;"/>
+		    <input type="reset" value="Reset Forms" style="margin: 5px;">
 		</form>
 	    </div>
 	
-	 
-	    
-	    
 	
     </div> <!--#wrapper -->
 
