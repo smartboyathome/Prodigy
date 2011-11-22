38e93f422afb5170f55087f4a8cd4bc4160fa374
Kei Kawaguchi <keikawaguchi@hotmail.com>
2011-11-21 19:17:59 -0800

    Needed to update, don't worry about this

--- classpage.php
+++ classpage.php
@@ -2,8 +2,23 @@
 
 
 
-    <div id="wrapper" class="frame">
+    <div id="wrapper" class="frame flc">
+        
+        <div id="classDetails" class="oneColumn">
+            <div class="module">
+                <h3 class="header">Class Title Here</h3>
+                <div class="smallPrint">Created By: Jessica Alba</div>
+                <div class="description">
+                    Food truck irony retro, whatever 8-bit yr lomo cred iphone leggings messenger bag tattooed lo-fi. Cred you probably haven't heard of them tofu, messenger bag artisan squid fixie freegan sartorial VHS ethical. Organic master cleanse scenester american apparel, williamsburg pitchfork helvetica. Williamsburg dreamcatcher quinoa, bicycle rights vinyl high life scenester stumptown letterpress american apparel keffiyeh. Brunch cred etsy viral. Hoodie gluten-free dreamcatcher quinoa beard. Helvetica pitchfork aesthetic food truck, irony blog single-origin coffee yr messenger bag bicycle rights biodiesel vinyl hoodie PBR.
+                </div>
+            </div>
+            
+            <div class="module">
+                <h3 class="header">Lessons</h3>
+            </div>
+        </div> <!-- .oneColumn -->
     
+    <!--
 	<div class="classpage">
 	    <div class="header">Class Name</div>
 		<div style="font-size: 15px; font-weight: bold;" align="left">Description:</div>
@@ -18,6 +33,7 @@
 	    </div>
 	    
 	</div>
+	-->
     </div> <!--#wrapper -->
 
     <footer id ="footer" class="frame">
