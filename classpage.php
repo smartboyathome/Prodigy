585b05aa02608fa8b6d63ba8cc7578a2bffd229a
Kei Kawaguchi <keikawaguchi@hotmail.com>
2011-11-21 19:43:53 -0800

    Classpage redesign

--- classpage.php
+++ classpage.php
@@ -4,19 +4,56 @@
 
     <div id="wrapper" class="frame flc">
         
-        <div id="classDetails" class="oneColumn">
-            <div class="module">
-                <h3 class="header">Class Title Here</h3>
-                <div class="smallPrint">Created By: Jessica Alba</div>
+        <div id="mainColumn">
+            <div class="module flc">
+                <h3 class="header">The Universe and the Origin of Life</h3>
+                <div class="smallPrint" style="margin-bottom: 10px;">Instructor: Jessica Alba</div>
                 <div class="description">
-                    Food truck irony retro, whatever 8-bit yr lomo cred iphone leggings messenger bag tattooed lo-fi. Cred you probably haven't heard of them tofu, messenger bag artisan squid fixie freegan sartorial VHS ethical. Organic master cleanse scenester american apparel, williamsburg pitchfork helvetica. Williamsburg dreamcatcher quinoa, bicycle rights vinyl high life scenester stumptown letterpress american apparel keffiyeh. Brunch cred etsy viral. Hoodie gluten-free dreamcatcher quinoa beard. Helvetica pitchfork aesthetic food truck, irony blog single-origin coffee yr messenger bag bicycle rights biodiesel vinyl hoodie PBR.
+                    Sequel to 101 or 102, emphasizing modern views of the atomic and molecular evolution of the universe from the initial "big bang" through the formation of the solar system and the emergence of biological forms on the earth. The latter part of the course considers questions about the existence of, and communication with, extraterrestrial intelligent life, and finally the ultimate fate of the cosmos. 
+                </div>
+                
+                <div class="fr">
+                    <a class="button">Enroll Now</a>
                 </div>
             </div>
             
             <div class="module">
-                <h3 class="header">Lessons</h3>
+                <h3 class="header" style="margin-bottom: 0;">Lessons</h3>
+                <article class="classDescription">
+                    <h4 class="title">Astrobiology</h4>
+                    <div class="smallPrint">Modified On: 12/12/12</div>
+                    <div class="description">Survey of the planets of the solar system, with emphases on recent space exploration of the planets and on the comparative evolution of the Earth and the other planets.</div>
+                </article>
+                
+                <article class="classDescription">
+                    <h4 class="title">Life in the Universe</h4>
+                    <div class="smallPrint">Modified On: 12/12/12</div>
+                    <div class="description">Survey of the planets of the solar system, with emphases on recent space exploration of the planets and on the comparative evolution of the Earth and the other planets.</div>
+                </article>
+                
+                <article class="classDescription">
+                    <h4 class="title">Origin of Life</h4>
+                    <div class="smallPrint">Modified On: 12/12/12</div>
+                    <div class="description">Survey of the planets of the solar system, with emphases on recent space exploration of the planets and on the comparative evolution of the Earth and the other planets.</div>
+                </article>
+                
+                <article class="classDescription">
+                    <h4 class="title">Exploring the Moon</h4>
+                    <div class="smallPrint">Modified On: 12/12/12</div>
+                    <div class="description">Survey of the planets of the solar system, with emphases on recent space exploration of the planets and on the comparative evolution of the Earth and the other planets.</div>
+                </article>
             </div>
-        </div> <!-- .oneColumn -->
+        </div> <!-- .mainColumn -->
+        
+        <div id="secondaryColumn">
+            <h4 class="header">What You'll Learn</h4>
+            <ul class="list">
+                <li><a href="">Astrobiology</a></li>
+                <li><a href="">Life in the Universe</a></li>
+                <li><a href="">Origin of Life</a></li>
+                <li><a href="">Exploring the Moon</a></li>
+            </ul>
+        </div>
     
     <!--
 	<div class="classpage">
