375d831eb5bf47c358ef83a43534e9d30c6ad008
smartboyathome <smartboyathome@gmail.com>
2011-12-03 21:44:42 -0800

    Here is home, didn't upload.

--- includes/home.php
+++ includes/home.php
@@ -10,8 +10,8 @@
 
 	echo("
 
-    <section class='classDescription'>
-         <h3 class='title'><a href='index.php?module=viewclass&classid=".$row['classID']."'>".$row['name']."</a></h3>
+    <section class='classDescription' data-link='index.php?module=viewclass&classid=".$row['classID']."'>
+         <h3 class='title'>".$row['name']."</h3>
         <div class='smallPrint'><span>".$row['enrolledCnt']." Users Enrolled</span> | Created on ");
     
     echo date('F jS Y \a\t g:i A', $row[createdDate]);
