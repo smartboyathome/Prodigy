1c6da2c7845fe0730620ffdc8015be2ec261abf1
picantegamer <picantegamer@gmail.com>
2011-12-06 23:59:09 -0800

    Fully functional classtools (still working on redirect)

--- login/classtools.php
+++ login/classtools.php
@@ -8,28 +8,49 @@ if(!isset($_POST['Submit']))
 include_once 'includes/header.php';
 $name = "";
 $desc = "";
-if(!empty($_GET['ClassId'])) 
+if(!empty($_GET['classid'])) 
 {
-    $db = new mysqlDAL;
-    $class = $db->getClass($_GET['ClassId']);
-    $name = $class['name'];
-    $desc = $class['description'];
+    $prodigyDB = new mysqlDAL;
+
+    foreach ($prodigyDB->getClass($_GET["classid"]) as $row){
+
+    $class = $_GET['classid'];
+    $name = $row['name'];
+    $desc = $row['description'];
+    }
 }
 ?>
 
     <div id="wrapper" class="frame">
-	    <div class="oneColumn">
-		<div align="center" class="classpage" style="height: auto;">
-		    <form name="input" action="classtools.php" method="post">
-			<div style="font-size: 20px; padding: 15px; font-weight: bold; color: #4DA400;">Welcome! Create a new class by entering a class name and description.</div>
-			Class Name: <input type="text" name="name" style="border-radius: 2px;" value="<?php if(!empty($name)) echo $name; ?>" /> <br> <br>
-			Description: <input type="text" size="80" name="description" style="border-radius: 2px;" value="<?php if(!empty($desc)) echo $desc; ?>" />
-			<br> <input type="submit" value="Create Class" style="margin: 5px;" name="Submit"/>
-		<?php if(!empty($_GET['ClassId'])) { ?> <input type="hidden" name="ClassId" value="<?php echo $_GET['ClassId']; ?>" /> <?php } ?>
-			<input type="reset" value="Reset Forms" style="margin: 5px;">
-		    </form>
-		</div>
-	</div>
+	    
+	    <div align="center" class="classpage" style="height: auto;">
+		<form name="input" action="classtools.php" method="post">
+		    <div style="font-size: 20px; padding: 15px; font-weight: bold; color: #4DA400;">
+
+            <?php
+            if (empty($_GET['classid'])){
+                echo("Create a new class by entering a class name and description");
+            }else{
+                echo("Editing the ".$name." class");
+            }
+            ?>
+            </div>
+		    Class Name: <input type="text" name="name" style="border-radius: 2px;" value="<?php if(!empty($name)) echo $name; ?>" /> <br> <br>
+		    Description: <input type="text" size="80" name="description" style="border-radius: 2px;" value="<?php if(!empty($desc)) echo $desc; ?>" />
+		    <br> 
+
+            <?php
+            if (empty($_GET['classid'])){
+                echo("<input type='submit' value='Create Class' style='margin: 5px;' name='Submit'/>");
+            }else{
+                echo("<input type='submit' value='Edit Class' style='margin: 5px;' name='Submit'/>");
+            }
+            ?>
+
+            <?php if(!empty($_GET['classid'])) { ?> <input type="hidden" name="ClassId" value="<?php echo $_GET['classid']; ?>" /> <?php } ?>
+		</form>
+	    </div>
+	
 	
     </div> <!--#wrapper -->
 <?php
@@ -42,10 +63,12 @@ else
         $db = new mysqlDAL;
         if(isset($_POST['ClassId']))
         {
+            echo("EDITING!");
             $db->editClass($_POST['ClassId'], $_POST['name'], $_POST['description']);
         }
         else
         {
+            echo("CREATING!!");
             $db->addClass($_POST['name'], $_POST['description']);
         }
     }
