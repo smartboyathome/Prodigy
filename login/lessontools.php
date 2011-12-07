36f131f58f355b421f330b1df61685f79c625c73
picantegamer <picantegamer@gmail.com>
2011-12-06 23:59:39 -0800

    Fully functional lesson tools.  Still working on redirect.

--- login/lessontools.php
+++ login/lessontools.php
@@ -7,34 +7,60 @@ if(!isset($_POST['Submit']))
 {
 include_once 'includes/header.php';
 $name = "";
-$desc = "";
-if(!empty($_GET['LessonId'])) 
-{
-    $db = new mysqlDAL;
-    $lesson = $db->getLesson($_GET['LessonId']);
-    $name = $lesson['name'];
-    $desc = $lesson['description'];
-    $cont = $lesson['content'];
-    $num = $lesson['lessonNum'];
+
+$prodigyDB = new mysqlDAL;
+if (!empty($_GET["lessonid"])){
+    $mode = "edit";
+    foreach ($prodigyDB->getLesson($_GET["lessonid"]) as $row){
+        $classID = $row['classID'];
+        $name = $row['name'];
+        $cont = $row['content'];
+        $num = $row['lessonNum'];
+    }
+}elseif (!empty($_GET["classid"])){
+    $mode = "new";
+    $classID = $_GET["classid"];
 }
+
+foreach ($prodigyDB->getClass($classID) as $row){
+    $classname = $row['name'];
+}
+
 ?>
 
     <div id="wrapper" class="frame">
 	    <div class="oneColumn">
 	    <div align="center" class="classpage" style="height: auto;">
-		<form name="input" action="classtools.php" method="post">
-		    <div style="font-size: 20px; padding: 15px; font-weight: bold; color: #4DA400;">Welcome! Create a new class by entering a class name and description.</div>
-		    Lesson Name: <input type="text" name="name" style="border-radius: 2px;" value="<?php if(!empty($name)) echo $name; ?>" /> <br> <br>
-		    Description: <input type="text" size="80" name="description" style="border-radius: 2px;" value="<?php if(!empty($desc)) echo $desc; ?>" /><br />
+		<form name="input" action="lessontools.php" method="post">
+		    <div style="font-size: 20px; padding: 15px; font-weight: bold; color: #4DA400;">
+
+            <?php
+            if ($mode == "new"){
+                echo("Creating a new lesson for the ".$classname." class");
+            }elseif ($mode == "edit"){
+                echo("Editing the lesson ".$name." from the ".$classname." class");
+            }
+            
+            ?> </div>
+		    Lesson Name: <input type="text" name="name" style="border-radius: 2px;" value="<?php 
+
+            if(!empty($name)) echo $name; ?>" /> <br>
             Lesson Number: <input type="text" size="3" name="number" style="border-radius: 2px;" value="<?php if(!empty($num)) echo $num; ?>" /><br />
+
+             <br>
             Content:<br />
-            <textarea name="content">
-                <?php if(!empty($cont)) echo $cont; ?>
-            </textarea>
-		    <br> <input type="submit" value="Create Lesson" style="margin: 5px;" name="Submit"/>
-            <?php if(!empty($_GET['LessonId'])) { ?> <input type="hidden" name="LessonId" value="<?php echo $_GET['LessonId']; ?>" /> <?php }
-            elseif(!empty($_GET['ClassId'])) { ?> <input type="hidden" name="ClassId" value="<?php echo $_GET['ClassId']; ?>" /> <?php } ?>
-		    <input type="reset" value="Reset Forms" style="margin: 5px;">
+            <textarea name="content" rows='20' cols='70'><?php if(!empty($cont)) echo $cont; ?></textarea>
+		    <br> 
+
+            <?php
+            if ($mode =="edit"){
+                echo("<input type='submit' value='Edit Lesson' style='margin: 5px;' name='Submit'/>");
+            }elseif ($mode=="new"){
+                echo("<input type='submit' value='Create Lesson' style='margin: 5px;' name='Submit'/>");
+            }
+            ?>
+            <?php if(!empty($_GET['lessonid'])) { ?> <input type="hidden" name="LessonId" value="<?php echo $_GET['lessonid']; ?>" /> <?php }
+            elseif(!empty($_GET['classid'])) { ?> <input type="hidden" name="ClassId" value="<?php echo $_GET['classid']; ?>" /> <?php } ?>
 		</form>
 	    </div>
 	    </div>
@@ -46,17 +72,22 @@ include_once 'includes/footer.php';
 }
 else
 {
-    if(!empty($_POST['name']) && !empty($_POST['description']) && !empty($_POST['content']))
+    //    public function addLesson($classID, $lessonNum, $name, $content){
+
+    if(!empty($_POST['name']) && !empty($_POST['number']) && !empty($_POST['content']))
     {
+
         $db = new mysqlDAL;
         if(isset($_POST['LessonId']))
         {
-            $db->editLesson($_POST['LessonId'], $_POST['name'], $_POST['description'], $_POST['content']);
+            $db->editLesson($_POST['LessonId'], $_POST['number'], $_POST['name'], $_POST['content']);
         }
-        else if(isset($_POST['ClassId']) && !empty($_POST['number']) && $db->lessonExists($_POST['ClassId'], $_POST['number']))
+        else if(isset($_POST['ClassId']))
         {
-            $db->addLesson($_POST['ClassId'], $_POST['number'], $_POST['name'], $_POST['description'], $_POST['content']);
+            $db->addLesson($_POST['ClassId'], $_POST['number'], $_POST['name'], $_POST['content']);
         }
+
+        header("Location: index.php");
     }
 }
 ?>
