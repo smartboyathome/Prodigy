610cc69c1ab84ced7c16fd820a1a9197121941e7
Mike Wiser <wiser.mike@gmail.com>
2011-12-06 18:39:27 -0800

    Working on enroll/unenroll

--- login/classtools.php
+++ login/classtools.php
@@ -18,18 +18,18 @@ if(!empty($_GET['ClassId']))
 ?>
 
     <div id="wrapper" class="frame">
-	    
-	    <div align="center" class="classpage" style="height: auto;">
-		<form name="input" action="classtools.php" method="post">
-		    <div style="font-size: 20px; padding: 15px; font-weight: bold; color: #4DA400;">Welcome! Create a new class by entering a class name and description.</div>
-		    Class Name: <input type="text" name="name" style="border-radius: 2px;" value="<?php if(!empty($name)) echo $name; ?>" /> <br> <br>
-		    Description: <input type="text" size="80" name="description" style="border-radius: 2px;" value="<?php if(!empty($desc)) echo $desc; ?>" />
-		    <br> <input type="submit" value="Create Class" style="margin: 5px;" name="Submit"/>
-            <?php if(!empty($_GET['ClassId'])) { ?> <input type="hidden" name="ClassId" value="<?php echo $_GET['ClassId']; ?>" /> <?php } ?>
-		    <input type="reset" value="Reset Forms" style="margin: 5px;">
-		</form>
-	    </div>
-	
+	    <div class="oneColumn">
+		<div align="center" class="classpage" style="height: auto;">
+		    <form name="input" action="classtools.php" method="post">
+			<div style="font-size: 20px; padding: 15px; font-weight: bold; color: #4DA400;">Welcome! Create a new class by entering a class name and description.</div>
+			Class Name: <input type="text" name="name" style="border-radius: 2px;" value="<?php if(!empty($name)) echo $name; ?>" /> <br> <br>
+			Description: <input type="text" size="80" name="description" style="border-radius: 2px;" value="<?php if(!empty($desc)) echo $desc; ?>" />
+			<br> <input type="submit" value="Create Class" style="margin: 5px;" name="Submit"/>
+		<?php if(!empty($_GET['ClassId'])) { ?> <input type="hidden" name="ClassId" value="<?php echo $_GET['ClassId']; ?>" /> <?php } ?>
+			<input type="reset" value="Reset Forms" style="margin: 5px;">
+		    </form>
+		</div>
+	</div>
 	
     </div> <!--#wrapper -->
 <?php
