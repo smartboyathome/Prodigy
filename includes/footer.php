8863db6597f08fa02688706d0a3c55af56897cef
keikawaguchi <keikawaguchi@hotmail.com>
2011-12-07 01:32:55 -0800

    Update includes/footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,26 +1,19 @@
     <footer id ="footer">
         <ul class="list">
             <li class="title">Prodigy.com &#8212;</li>
-            <li><a href="">What is Prodigy?</a></li>
-            <li><a href="">Features</a></li>
+            <li><a href="index.php?module=gettingstarted">Getting Started</a></li>
         </ul>
         
         <ul class="list">
             <li class="title">Site Map &#8212;</li>
             <li><a href="/">Home</a></li>
             <li><a href="index.php?module=viewcatalog">Browse</a></li>
-            <li><a href="">Subjects</a></li>
         </ul>
         
         <ul class="list">
             <li class="title">Account &#8212;</li>
             <li><a href="account.php">Profile</a></li>
             <li><a href="index.php?module=viewcatalog">Classes</a></li>
-        </ul>
-        
-        <ul class="list">
-            <li class="title">Suggestions? &#8212;</li>
-            <li><a href="">Contact Us</a></li>
         </ul>
     </footer>
 
